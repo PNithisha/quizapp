a4f5049d5f02a3f1eb7c28b029eb7884837fcefc
PNithisha <105432560+PNithisha@users.noreply.github.com>
2023-09-26 22:50:10 +0530

    Add files via upload

---
+++ questionAnswer.java
@@ -0,0 +1,28 @@
+package com.example.quizapp;
+
+public class questionAnswer {
+
+    public static String questionss[]= {
+            "What is the capital of india?",
+            "What planet is closest to the sun?",
+            "How many hearts does an octopus have?",
+            "What is the richest temple in India?",
+            "Who is the first Indian to win a Nobel Prize?"
+    };
+
+    public static  String choices[][] = {
+            { "Chennai" , "Delhi" , "Hyderabad" , "Mumbai"},
+            {"Mars" , "Saturn" , "Earth"  , "Mercury"} ,
+            {"one" , "two" , "three" , "four" },
+            {"Tirupati" , "Golden Temple" , "Padmanabhaswamy" , "Shirdi Sai Baba"},
+            {"Mother Teresa" , "Sir C V Raman" , "Rabindranath Tagore" , "Har Gobind Khorana"}
+    };
+
+    public static String CorrectAnswer[] = {
+            "Delhi",
+            "Mercury",
+            "three",
+            "Padmanabhaswamy",
+            "Rabindranath Tagore"
+};
+}
